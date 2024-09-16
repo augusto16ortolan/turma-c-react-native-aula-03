40058d2ce08470fcb2b6cb6a3d3c1fb4ca47afed
Augusto <augustoortolan02@gmail.com>
2024-09-16 20:05:10 -0300

    Função map

--- aula03.js
+++ aula03.js
@@ -16,3 +16,68 @@ const numerosImpares = numeros.filter((numero) => numero % 2 != 0);
 
 /*console.log("Pares", numerosPares);
 console.log("Impares", numerosImpares);*/
+
+/*const numerosMultiplicados = [];
+for (var numero of numeros) {
+  const resultado = numero * 2;
+  numerosMultiplicados.push(resultado);
+}*/
+
+const pares = numeros.filter((numero) => {
+  return numero % 2 == 0;
+});
+const numerosMultiplicados = numeros.map((numero) => numero);
+
+//console.log(numerosMultiplicados);
+
+const pessoas = [
+  {
+    nome: "Luiz",
+    sobrenome: "Souza",
+    idade: 18,
+  },
+  {
+    nome: "Augusto",
+    sobrenome: "Ortolan",
+    idade: 18,
+  },
+  {
+    nome: "Ana",
+    sobrenome: "Lima",
+    idade: 16,
+  },
+  {
+    nome: "Bernardo",
+    sobrenome: "Santos",
+    idade: 23,
+  },
+  {
+    nome: "Laura",
+    sobrenome: "Ribeiro",
+    idade: 15,
+  },
+];
+
+const pessoasMaioresDeIdade = pessoas
+  .filter((pessoa) => {
+    return pessoa.idade >= 18;
+  })
+  .map((pessoa) => {
+    const nomeCompleto = pessoa.nome + " " + pessoa.sobrenome;
+    return nomeCompleto;
+  });
+
+//console.log(pessoasMaioresDeIdade);
+
+const frutas = [
+  { nome: "Maça", podeFazerSuco: true },
+  { nome: "Melancia", podeFazerSuco: true },
+  { nome: "Laranja", podeFazerSuco: true },
+  { nome: "Banana", podeFazerSuco: false },
+  { nome: "Abacate", podeFazerSuco: false },
+];
+
+const sucos = frutas
+  .filter((fruta) => fruta.podeFazerSuco)
+  .map((fruta) => `Suco de ${fruta.nome}`)
+  .forEach((fruta) => console.log(fruta));
